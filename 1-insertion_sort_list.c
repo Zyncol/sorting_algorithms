e7f76a8dd7a8a25d8cf09f2b721a83504b00360c
Zyncol <isaacyilipochipofya@gmail.com>
2024-01-28 02:57:50 +0200

    just further

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,56 @@
+#include "sort.h"
+
+/**
+ *len_list - returns the length of a linked list
+ *@h: pointer to the list
+ *Return: length of list
+ **/
+int len_list(listint_t *h)
+{
+int kuta = 0;
+while (h != NULL)
+{
+kuta++;
+h = h->next;
+}
+return (kuta);
+}
+/**
+ * insertion_sort_list - uses an insertion alorithm
+ * @list: a pointer to the list
+ **/
+void insertion_sort_list(listint_t **list)
+{
+listint_t *pano = NULL, *fst = NULL;
+listint_t *scnd = NULL;
+listint_t *ird = NULL;
+listint_t *rt = NULL;
+if (!list || !(*list) || len_list(*list) < 2)
+return;
+pano = *list;
+while (pano != NULL)
+{
+if (pano->prev && pano->n < pano->prev->n)
+{
+fst = pano->prev->prev;
+scnd = pano->prev;
+ird = pano;
+rt = pano->next;
+scnd->next = rt;
+if (rt)
+rt->prev = scnd;
+ird->next = scnd;
+ird->prev = fst;
+if (fst)
+fst->next = ird;
+else
+*list = ird;
+scnd->prev = ird;
+pano = *list;
+print_list(*list);
+continue;
+}
+else
+pano = pano->next;
+}
+}
--- sort.h
+++ sort.h
@@ -21,4 +21,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 int list_leng(listint_t *pd);
+void selection_sort(int *array, size_t size);
+void swapps_swapping(int *r, int *o);
 #endif
